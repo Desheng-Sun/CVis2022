7a5a1cb881fb9d3f18d4ff63337f6a568967fe60
gaogaogaogao0430 <1123263949@qq.com>
2022-05-20 19:38:10 +0800

    修改difchart布局的逻辑以及数据格式

--- vis/src/components/dif-chart/index.jsx
+++ vis/src/components/dif-chart/index.jsx
@@ -13,8 +13,15 @@ export default function DifChart({ w, h }) {
     setSvgHeight(h);
   }, [h]);
   useEffect(() => {
-    for (let i = 0; i < 5; i++) {
-      drawChart(i);
+    let n = 5;
+    let svg = d3
+      .select("#difference-chart")
+      .append("svg")
+      .attr("id", "all-svg")
+      .attr("width", svgWidth)
+      .attr("height", n * svgHeight * 1.1);
+    for (let i = 0; i < n; i++) {
+      drawChart(i, svg);
     }
   });
   function getRectPosition(rectWidth, rectPadding) {
@@ -27,56 +34,84 @@ export default function DifChart({ w, h }) {
   }
 
   // 绘制结构图
-  function drawChart(i) {
+  function drawChart(i, svg) {
     const height = 400;
     const width = svgWidth;
-    let dataset = [10, 20, 35, 40, 52];
-    let dataset2 = [80, 40, 35, 20, 10];
-    let rectWidth = [50, 100, 120, 50, 120]; //每个矩形的默认宽度
+    const difdata = [
+      {
+        name: "IP_1", value:
+          [{ name: 'A', value: 10 },
+          { name: 'A,B', value: 20 },
+          { name: 'B,C', value: 35 },
+          { name: 'B,D,E', value: 40 },
+          { name: 'B,D', value: 52 }
+          ]
+      },
+      {
+        name: 'IP_2', value:
+          [{ name: 'A', value: 80 },
+          { name: 'A,B', value: 40 },
+          { name: 'B,C', value: 35 },
+          { name: 'B,D,E', value: 20 },
+          { name: 'B,D', value: 10 }]
+      },
+      {
+        name: "IP_1+IP_2", value:
+          [{ name: 'A', value: 50 },
+          { name: 'A,B', value: 100 },
+          { name: 'B,C', value: 120 },
+          { name: 'B,D,E', value: 50 },
+          { name: 'B,D', value: 120 }]
+      }];
+    let dataset1 = [];
+    Object.values(difdata[0].value).forEach(val => { dataset1.push(val.value) });
+    let dataset2 = [];
+    Object.values(difdata[1].value).forEach(val => { dataset2.push(val.value) });
+    let rectWidth = []; //每个矩形的默认宽度
+    Object.values(difdata[2].value).forEach(val => { rectWidth.push(val.value) });
     let rectPadding = 10; //每个矩形间的间隔
     let rectX = getRectPosition(rectWidth, rectPadding);
-    let padding = { top: 30, bottom: 30, left: 30, right: 40 }; //定义间隔
+    let padding = { top: 30, bottom: 30, left: 30, right: 30 }; //定义间隔
     //定义画布
-    let svg = d3
-      .select("#difference-chart")
-      .append("svg")
+    let g = svg
+      .append("g")
       .attr("class", "dif-chart" + i)
+      .attr("transform", `translate(${padding.left},${svgHeight * i * 1.1})`)
       .attr("width", width)
       .attr("height", height);
 
     //定义矩形比例尺
     let yScale1 = d3
       .scaleLinear()
-      .domain([0, d3.max([d3.max(dataset), d3.max(dataset2)])])
+      .domain([0, d3.max([d3.max(dataset1), d3.max(dataset2)])])
       .range([height / 2 - padding.top, 0]);
     let yAxis1 = d3.axisLeft(yScale1).ticks(5);
     let yScale2 = d3
       .scaleLinear()
-      .domain([-d3.max([d3.max(dataset), d3.max(dataset2)]), 0])
+      .domain([-d3.max([d3.max(dataset1), d3.max(dataset2)]), 0])
       .range([height - 2 * padding.top, height / 2 - padding.top]);
     let yAxis2 = d3.axisLeft(yScale2).ticks(5);
-    svg
+    g
       .append("g")
       .attr("transform", `translate(${padding.top},${padding.left})`)
       .call(yAxis1);
-    svg
+    g
       .append("g")
       .attr("transform", `translate(${padding.top},${padding.left})`)
       .call(yAxis2);
     let xScale = d3.scaleOrdinal().domain([0, 1, 2, 3, 4, 5]).range(rectX);
     let xAxis = d3.axisBottom(xScale).ticks(5);
-    svg
+    g
       .append("g")
       .attr("transform", `translate(${padding.left},${height / 2})`)
       .call(xAxis);
-
     //定义矩形
     let g1 = d3
       .selectAll(".dif-chart" + i)
       .append("g")
       .attr("transform", `translate(${padding.top},${padding.left})`);
 
-    let graph1 = g1.selectAll("rect").data(dataset).enter().append("g");
+    let graph1 = g1.selectAll("rect").data(dataset1).enter().append("g");
 
     graph1
       .append("rect")
