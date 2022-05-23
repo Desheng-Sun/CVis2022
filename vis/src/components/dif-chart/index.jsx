b70ff1032f5d8d944164162a7a95fe5fc900cdc9
奥利奥 <592041751@qq.com>
2022-05-23 22:47:55 +0800

    基于旭日图重新绘制了dif

--- vis/src/components/dif-chart/index.jsx
+++ vis/src/components/dif-chart/index.jsx
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import { useEffect, useState } from "react";
 import "../dif-chart/index.css";
+
 export default function DifChart({ w, h }) {
   const [svgWidth, setSvgWidth] = useState(w);
   const [svgHeight, setSvgHeight] = useState(h);
@@ -20,140 +21,154 @@ export default function DifChart({ w, h }) {
 
   // 绘制结构图
   function draw() {
-    let data = [
-      {
-        group: "IP1",
-        relatednum: 2,
-        relateddata:
-          [{
-            name: "IP3",
-            samevalue:
-              [{ name: "A", value: 1 },
-              { name: "AB", value: 0 },
-              { name: "ABE", value: 0 },
-              { name: "B", value: 0 }],
-            fathervalue:
-              [{ name: "A", value: 1 },
-              { name: "AB", value: 2 },
-              { name: "ABE", value: 0 },
-              { name: "B", value: 0 }],
-            sonvalue:
-              [{ name: "A", value: 1 },
-              { name: "AB", value: 2 },
-              { name: "ABE", value: 1 },
-              { name: "B", value: 2 }],
+    let radius = svgWidth / 2;
 
-          },
-          {
-            name: "IP4",
-            samevalue:
-              [{ name: "B", value: 3 },
-              { name: "ABE", value: 0 },
-              { name: "AB", value: 0 },
-              { name: "BC", value: 0 },],
-            fathervalue:
-              [{ name: "ABE", value: 5 },
-              { name: "AB", value: 2 },
-              { name: "BC", value: 0 },
-              { name: "B", value: 0 }],
-            sonvalue:
-              [{ name: "BC", value: 1 },
-              { name: "B", value: 2 }, 
-              { name: "ABE", value:0 },
-              { name: "AB", value: 0 }],
+    let rawdata = {
+      name: "root",
+      children: [
+        {
+          name: "IP_1", children: [
+            {
+              name: "IP_2", children: [
+                { name: "A", num: 4, children: [{ name: "B", num: 0, children: [{ name: "C", num: 6, value: 5 }] }] },//IP1自己的黑灰产数量num value都是5
+                { name: "A", num: 0, children: [{ name: "B", num: 3, children: [{ name: "C", num: 1, value: 5 }] }] },//IP和IP2共有的黑灰产数量
+                { name: "A", num: 2, children: [{ name: "B", num: 0, children: [{ name: "C", num: 2, value: 5 }] }] }]//IP2自己的黑灰产数量
+            },
+            {
+              name: "IP_3", children: [
+                { name: "AB", num: 0, children: [{ name: "BC", num: 1, children: [{ name: "CE", num: 0, value: 5 }] }] },
+                { name: "AB", num: 1, children: [{ name: "BC", num: 3, children: [{ name: "CE", num: 2, value: 5 }] }] },
+                { name: "AB", num: 4, children: [{ name: "BC", num: 3, children: [{ name: "CE", num: 1, value: 5 }] }] }]
+            }]
+        },
+        {
+          name: "IP_4", children: [{
+            name: "IP_5", children: [
+              { name: "AE", num: 4, children: [{ name: "B", num: 0, value: 5 }] },//IP1自己的黑灰产数量num value都是5
+              { name: "AE", num: 0, children: [{ name: "B", num: 3, value: 5}] },//IP和IP2共有的黑灰产数量
+              { name: "AE", num: 2, children: [{ name: "B", num: 0, value: 5}] }]//IP2自己的黑灰产数量
           }]
-      },
-      { group: "IP2", relatednum: 3 },
-      { group: "Cert1", relatednum: 2 },
-      { group: "IP3", relatednum: 7 },
-      { group: "Cert2", relatednum: 3 },
-      { group: "IP4", relatednum: 5 }]
+        },
 
+      ]
+    }
+    function partition(data) {
+      return (d3.partition().size([2 * Math.PI, radius * radius])(
+        d3
+          .hierarchy(data)
+          .sum(d => d.value)
+          .sort((a, b) => b.value - a.value)
+      )
+      )
+    }
+    const root = partition(rawdata);
+    // console.log(root);
+    // console.log(root.descendants())
+    let color = d3
+      .scaleOrdinal()
+      .domain(d3.range(6))
+      .range(["#5d85cf", "#7c6561", "#da7847", "#6fb971", "#9e70cf", "#bbbbbb"])
     let svg = d3
       .select("#difference-chart")
       .append("svg")
-      .attr("viewBox", [-svgWidth / 2, -svgHeight / 2, svgWidth, svgHeight]);
+      .attr('width', svgWidth)
+      .attr('height', svgHeight);
+    // let element = svg.node();
+    // element.value = { sequence: [], percentage: 0.0 };
 
-    let color = d3
-      .scaleOrdinal()
-      .domain(data.map(d => d.group))
-      .range(
-        d3
-          .quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length)
-          .reverse()
-      );
+    // let label = svg
+    //   .append("text")
+    //   .attr("text-anchor", "middle")
+    //   .attr("fill", "#888")
+    //   .style("visibility", "hidden");
 
+    // label
+    //   .append("tspan")
+    //   .attr("class", "percentage")
+    //   .attr("x", 0)
+    //   .attr("y", 0)
+    //   .attr("dy", "-0.1em")
+    //   .attr("font-size", "3em")
+    //   .text("");
 
-    let IPsumdata = data.forEach(d => d.relatednum);
-    console.log(IPsumdata)
-    //绘制内部扇形
-    let pie = d3
-      .pie()
-      .padAngle(0)
-      .sort(null)
-      .value(d => d.relatednum);
-    let arcs = pie(data);
+    // label
+    //   .append("tspan")
+    //   .attr("x", 0)
+    //   .attr("y", 0)
+    //   .attr("dy", "1.5em")
+    //   .text("of visits begin with this sequence");
+
+    svg
+      .attr("viewBox", `${-radius} ${-radius} ${svgWidth} ${svgWidth}`)
+      .style("max-width", `${svgWidth}px`)
+      .style("font", "12px sans-serif");
     let arc = d3
       .arc()
-      .innerRadius(0)
-      .outerRadius(Math.min(svgWidth, svgHeight) / 6 - 1);
-    svg
-      .append("g")
-      .selectAll("path")
-      .data(arcs)
-      .join("path")
-      .attr("fill", d => color(d.data.group))
-      .attr("d", arc)
-      .append("title")
-      .text(d => `${d.data.group}: ${d.data.relatednum.toLocaleString()}`);
-    let radius = (Math.min(svgWidth, svgHeight) / 6) * 0.7;
-    let arcLabel = d3
-      .arc()
-      .innerRadius(radius)
-      .outerRadius(radius);
-    svg
-      .append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 12)
-      .attr("text-anchor", "middle")
-      .selectAll("text")
-      .data(arcs)
-      .join("text")
-      .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
-      .call(text =>
-        text
-          .append("tspan")
-          .attr("y", "-0.4em")
-          .attr("font-weight", "bold")
-          .text(d => d.data.group)
+      .startAngle(d => d.x0)
+      .endAngle(d => d.x1)
+      .padAngle(1 / radius)
+      .cornerRadius(60)
+      // .padRadius(0.9)
+      .innerRadius(d => {
+
+        if (d.depth === 1) return 0
+        return Math.sqrt(d.y0)
+      })
+      .outerRadius(d => {
+        if (!d.depth) return 0
+        return Math.sqrt(d.y1) - 1
+      }
       )
-      .call(text =>
-        text
-          .filter(d => d.endAngle - d.startAngle > 0.25)
-          .append("tspan")
-          .attr("x", 0)
-          .attr("y", "0.7em")
-          .attr("fill-opacity", 0.7)
-          .text(d => d.data.relatednum.toLocaleString())
-      );
-
-
-    //绘制产业信息
-    let industryarc = d3
-      .arc()
-      .startAngle(0)
-      .endAngle(Math.PI / 2)
-      .innerRadius(Math.min(svgWidth, svgHeight) / 6 + 3)
-      .outerRadius(Math.min(svgWidth, svgHeight) / 3 - 1);
-    svg
+    const path = svg
       .append("g")
       .selectAll("path")
-      .data(arcs)
+      .data(root.descendants())
       .join("path")
-      .attr("fill", d => color(d.data.group))
-      .attr("d", industryarc)
-      .append("title");
-    // .text(d => `${d.data.group}: ${d.data.relatednum.toLocaleString()}`);
+      .attr("fill", d => {
+        if (undefined == d.data.num) return "#9e70cf"
+        return color(d.data.num)
+      })
+      .attr("d", arc);
+
+    // svg
+    //   .append("g")
+    //   .attr("fill", "none")
+    //   .attr("pointer-events", "all")
+    //   .on("mouseleave", () => {
+    //     path.attr("fill-opacity", 1);
+    //     label.style("visibility", "hidden");
+    //     // Update the value of this view
+    //     element.value = { sequence: [], percentage: 0.0 };
+    //     element.dispatchEvent(new CustomEvent("input"));
+    //   })
+    //   .selectAll("path")
+    //   .data(
+    //     root.descendants().filter(d => {
+    //       // Don't draw the root node, and for efficiency, filter out nodes that would be too small to see
+    //       return d.depth && d.x1 - d.x0 > 0.001;
+    //     })
+    //   )
+    //   .join("path")
+    //   .attr("d", mousearc)
+    //   .on("mouseenter", (event, d) => {
+    //     // Get the ancestors of the current segment, minus the root
+    //     let sequence = d
+    //       .ancestors()
+    //       .reverse()
+    //       .slice(1);
+    //     // Highlight the ancestors
+    //     path.attr("fill-opacity", node =>
+    //       sequence.indexOf(node) >= 0 ? 1.0 : 0.3
+    //     );
+    //     const percentage = ((100 * d.value) / root.value).toPrecision(3);
+    //     label
+    //       .style("visibility", null)
+    //       .select(".percentage")
+    //       .text(percentage + "%");
+    //     // Update the value of this view with the currently hovered sequence and percentage
+    //     element.value = { sequence, percentage };
+    //     element.dispatchEvent(new CustomEvent("input"));
+    //   });
   }
 
 
